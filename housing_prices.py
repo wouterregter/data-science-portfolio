c957953d09a4fb453affb149f537f7599d2ff479
wouterregter <wouterregter@hotmail.com>
2020-11-18 21:59:19 +0100

    Update housing_prices.py

--- housing_prices.py
+++ housing_prices.py
@@ -1,29 +1,37 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
 
 # import the train data
-df = pd.read_csv('train.csv')
+df_train = pd.read_csv('train.csv')
+df_test = pd.read_csv('test.csv')
 
 # inspect the data
-df.info()
-df.describe()
-df.head()
+df_train.info()
+df_train.describe()
+df_train.head()
+
+# get dummies for categorical variables
+df_train = pd.get_dummies(df)
 
 # set predictors and response
-X = df.drop(['Id', 'SalePrice'], axis = 1).values
-y = df['SalePrice'].values
+X_train = df_train.drop(['Id', 'SalePrice'], axis = 1).values
+y_train = df_train['SalePrice'].values
+X_test = df_test.drop(['Id', 'SalePrice'], axis = 1).values
+y_test = df_test['SalePrice'].values
 
 # check for missing values
 df.isna().sum().sum()
 
-# encode dummies
-X = pd.get_dummies(X)
-
 # impute missing values
 imp = SimpleImputer(missing_values=np.nan, strategy='mean') # instantiate imputer
-imp.fit(X)
-X = imp.transform(X) # impute means
+imp.fit(X) # fit imputer
+X_train = imp.transform(X_train) # impute means
+
+reg = LinearRegression()
+reg.fit()
+
 
 
 
